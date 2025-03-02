bf6c1bd4ab5929c409a931f897e6eb665f7e0be6
AndresFVA <andresf79@gmail.com>
2025-03-02 17:33:57 -0500

    Se envia secuencia que indica pantalla encendida y lista

--- USER/main.c
+++ USER/main.c
@@ -3,6 +3,7 @@
 #include "uart.h"
 #include "ui.h"
 
+u8 initSring[9]={0x5A, 0xA5, 0x06, 0x83, 0x10, 0x00, 0x01, 0x00, 0x01};
 
 void main()
 {
@@ -13,6 +14,8 @@ void main()
   Uart2_Init(115200, MODE_8N1);
 	pageNumber=50;//Para que empiece en esta pagina diferente al 0 por defecto
 	Page_Change(pageNumber);
+	//Se envia la secuencia que indica que la pantalla esta encendida y lista
+	Uart2_Sendstring(initSring, 9);
 	while(1)
   {
 		uartRun(&uart2, Analyze_8283);
